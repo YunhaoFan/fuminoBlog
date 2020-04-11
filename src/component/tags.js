5381c8d115a1aca69190af47e50a4c91c40587d9
fumino <274212303@qq.com>
2020-04-11 21:57:06 +0800

    侧边导航适配移动端，增加收起展开功能

--- src/component/tags.js
+++ src/component/tags.js
@@ -8,7 +8,8 @@ class Tags extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			tagsElemArr: []
+			tagsElemArr: [],
+			showNav:''
 		};
 	}
 
@@ -16,8 +17,9 @@ class Tags extends Component {
 		return (
 			<div className="tag-container" ref={'tags'}>
 				{this.state.tagsElemArr}
+				{this.state.showNav}
+				<div className="tag-hidden" ref={'navHidden'} onClick={()=>this.handleClickNavHidden()}>收起来!</div>
 			</div>
-
 		);
 	}
 
@@ -47,7 +49,27 @@ class Tags extends Component {
 		this.props.abstract.getAbstractByTag(item);
 	}
 
-	// 绑定给tags container绑定拖动事件
+	// 单击收起事件
+	handleClickNavHidden(){
+		const tagContainer =this.refs.tags;
+		const tagHidden = this.refs.navHidden;
+		tagContainer.classList.remove('tag-container');
+		tagContainer.classList.add('tag-container-hidden');
+		this.setState({'showNav':(<div className='show-nav' onClick={()=>this.handleClickNavShown()}>展开!</div>)});
+		tagHidden.style.display = 'none';
+	}
+
+	//单击展开事件
+	handleClickNavShown(){
+		const tagContainer =this.refs.tags;
+		const tagHidden = this.refs.navHidden;
+		tagContainer.classList.add('tag-container');
+		tagContainer.classList.remove('tag-container-hidden');
+		this.setState({'showNav':''});
+		tagHidden.style.display = 'block';
+	}
+
+	// 给tags container绑定鼠标拖动事件
 	dragTagGroup() {
 		const tagContainer = this.refs.tags;
 		let startX, startY, moveX, moveY, mousedownFlag, originTop, originLeft;
@@ -76,9 +98,41 @@ class Tags extends Component {
 		})
 	}
 
+	// 给tags container绑定触屏拖动事件
+	touchDragTagGroup() {
+		const tagContainer = this.refs.tags;
+		let startX, startY, moveX, moveY, mousedownFlag, originTop, originLeft;
+		tagContainer.addEventListener('touchstart', (e) => {
+			tagContainer.style.cursor = 'grabbing';
+			const touch = e.targetTouches[0];
+			startX = touch.clientX;
+			startY = touch.clientY;
+			console.log('x,y:', startX, startY);
+			mousedownFlag = 1;
+			originTop = getTop(tagContainer);
+			originLeft = getLeft(tagContainer);
+		});
+		window.addEventListener('touchmove', (e) => {
+			const touch = e.targetTouches[0];
+			moveX = touch.clientX - startX;
+			moveY = touch.clientY - startY;
+			if (mousedownFlag === 1) {
+				console.log('mx,my:', moveX, moveY);
+				console.log(getTop(tagContainer), getLeft(tagContainer));
+				tagContainer.style.top = originTop + moveY + 'px';
+				tagContainer.style.left = originLeft + moveX + 'px';
+			}
+			e.preventDefault();
+		},{ passive: false });
+		window.addEventListener('touchend', (e) => {
+			mousedownFlag = 0;
+			tagContainer.style.cursor = 'grab';
+		})
+	}
 	componentDidMount() {
 		this.getAllTags();
 		this.dragTagGroup();
+		this.touchDragTagGroup();
 	}
 }
 
